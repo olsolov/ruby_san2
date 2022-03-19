c00a46bcd1713c03dc879165a0be913f43a7aecc
Olga Soloveva <olyaaa.solo@gmail.com>
2022-03-19 23:31:39 +0100

    Add lesson 4, operators

---
+++ 1/operators.rb
@@ -0,0 +1,25 @@
+name = 'Sasha'
+add = 1 + 2
+sub = 2 - 1
+mult = 2 * 2
+div = 2 / 2
+
+eq_1 = 1 < 2
+eq_2 = 1 > 2
+eq_3 = 1 == 2
+eq_4 = 2 == 2
+eq_5 = 2 >= 2
+eq_6 = 2 <= 2
+
+puts name
+puts add
+puts sub
+puts mult
+puts div
+puts eq_1
+puts eq_2
+puts eq_3
+puts eq_4
+puts eq_5
+puts eq_6
+puts "#{eq_5}" + ' ' + "#{eq_6}"
