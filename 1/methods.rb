fb36ab469f2360e98658de3b9031300e01f46ad9
Olga Soloveva <olyaaa.solo@gmail.com>
2022-03-26 22:06:19 +0100

    Add lesson 8, methods

---
+++ 1/methods.rb
@@ -0,0 +1,15 @@
+def division_1
+  puts 'method body'
+end
+
+def division_2(name)
+  puts 'My name is ' + name
+end
+
+def division_3
+  name = 'Dasha'
+  division_1
+  division_2(name)
+end
+
+division_3
