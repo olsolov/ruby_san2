e9001836113ec97781e68c7d6e91522c5afe2851
Olga Soloveva <olyaaa.solo@gmail.com>
2022-03-26 22:28:00 +0100

    Add lesson 9

--- 1/methods.rb
+++ 1/methods.rb
@@ -1,15 +1,11 @@
-def division_1
-  puts 'method body'
+def division_2(name = "", age = nil, weight = 71)
+  puts 'My name is '   + name
+  puts 'My age is '    + age.to_s
+  puts 'My weight is ' + "#{weight}"
+  puts '________'
 end
 
-def division_2(name)
-  puts 'My name is ' + name
-end
-
-def division_3
-  name = 'Dasha'
-  division_1
-  division_2(name)
-end
-
-division_3
+division_2('Dasha', 21)
+division_2('Pasha', 25, 78)
+division_2('Sasha', 35, 70)
+division_2 'Anton'
