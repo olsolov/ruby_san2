a48236d5eb2b8e0167b6042e4fa4f08c3b9f78c4
Olga Soloveva <olyaaa.solo@gmail.com>
2022-03-27 20:21:20 +0200

    Add lesson 13, attribute readers, writers and accessors

--- 2/item.rb
+++ 2/item.rb
@@ -1,22 +1,27 @@
 class Item
   def initialize
-    @price = 30
+    @price = 300
+    @height = 300
   end
 
-  def price
-    @price
-  end
+  # attr_reader :price, :height
+  # attr_writer :price
 
-  def price=(value)
-    @price = value
-  end
+  attr_accessor :price, :height
+
 end
 
 item1 = Item.new
 item2 = Item.new
 
-#item1.price = 10
+item1.price = 10
 item2.price = 20
 
+item1.height = 10
+item2.height = 30
+
 p item1.price
+p item1.height
+
 p item2.price
+p item2.height
