7017666290f76363d7088a595c8332589499338d
Olga Soloveva <olyaaa.solo@gmail.com>
2022-03-27 11:46:53 +0200

    Add lesson 12, getters and setters

--- 2/item.rb
+++ 2/item.rb
@@ -1,12 +1,22 @@
 class Item
-  name = 'Dasha'
+  def initialize
+    @price = 30
+  end
 
   def price
-    rand 100
+    @price
+  end
+
+  def price=(value)
+    @price = value
   end
 end
 
 item1 = Item.new
 item2 = Item.new
+
+#item1.price = 10
+item2.price = 20
+
 p item1.price
 p item2.price
