e5aa08905a338f0e84f4bd65791263c797efd821
Olga Soloveva <olyaaa.solo@gmail.com>
2022-03-27 22:53:16 +0200

    Add lesson 15, array

--- 2/item.rb
+++ 2/item.rb
@@ -1,15 +1,10 @@
 class Item
-  def initialize(options)
+
+  attr_accessor :price, :weight
+
+  def initialize(options = {})
     @price = options[:price]
     @weight = options[:weight]
   end
 
-  attr_accessor :price, :weight
-
 end
-
-item1 = Item.new({weight: 20, price: 50})
-item2 = Item.new({weight: 60, price: 30})
-
-p item1
-p item2
