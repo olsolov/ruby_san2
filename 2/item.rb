66df8b922ebf7aae16ffef72a3660bc1eb1ee163
Olga Soloveva <olyaaa.solo@gmail.com>
2022-03-27 21:15:54 +0200

    Add lesson 14, hash

--- 2/item.rb
+++ 2/item.rb
@@ -1,27 +1,15 @@
 class Item
-  def initialize
-    @price = 300
-    @height = 300
+  def initialize(options)
+    @price = options[:price]
+    @weight = options[:weight]
   end
 
-  # attr_reader :price, :height
-  # attr_writer :price
-
-  attr_accessor :price, :height
+  attr_accessor :price, :weight
 
 end
 
-item1 = Item.new
-item2 = Item.new
+item1 = Item.new({weight: 20, price: 50})
+item2 = Item.new({weight: 60, price: 30})
 
-item1.price = 10
-item2.price = 20
-
-item1.height = 10
-item2.height = 30
-
-p item1.price
-p item1.height
-
-p item2.price
-p item2.height
+p item1
+p item2
