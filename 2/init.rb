ae331336f33c74c0895f81e6429f864437cfa2f2
Olga Soloveva <olyaaa.solo@gmail.com>
2022-03-29 13:52:10 +0200

    Add lesson 18

--- 2/init.rb
+++ 2/init.rb
@@ -1,7 +1,17 @@
 require_relative 'cart'
 require_relative 'item'
 
-item = Item.new({price: 25, weight: 120, name: 'Car'})
 item1 = Item.new({price: 25, weight: 120, name: 'Car'})
-item.info { |value| puts value }
-item1.info
+item2 = Item.new({weight: 120, name: 'Car'})
+
+cart = Cart.new
+cart.add_item item1
+cart.add_item item2
+
+p cart.items
+
+cart.delete_invalid_items
+
+puts '______'
+
+p cart.items
