e9eea945f9019159a9cf38f94b92853418b793b6
Olga Soloveva <olyaaa.solo@gmail.com>
2022-04-02 20:07:21 +0200

    Add lesson 26

--- 1/ruby_tutorial.rb
+++ 1/ruby_tutorial.rb
@@ -26,11 +26,54 @@ class RubyTutorial
     p 1.to_f
     p 1.to_s
     p 1.to_i
-    
+
     p '_____'
     p rand(1..10)
     p rand(1...10)
     p '_____'
+
+    p 'SASHA'.downcase
+    p 'sasha'.upcase
+    p 'mo lo ko'.capitalize
+    p '_____'
+
+    array2 = %w[sasha masha]
+
+    string = "sasha masha"
+    p string.chars
+    p '_____'
+
+    p string.split(',')
+    p '_____'
+
+    array = %w[sasha dasha pasha]
+    p array.find_index('pasha')
+    p '_____'
+
+    array = [1, 20, 3, 4]
+    array.find_all { |num| p num.odd? unless num.nil? }
+    p '_____'
+    array.find_all { |num| p true }
+    p '_____'
+    array.find { |num| p num.even? }
+    p '_____'
+    array.map{ |str| p str.to_s.length }
+    p '_____'
+
+    hash = { car: 'audi', name: 'a8', engine: 'v12'}
+    p hash.reverse_each.to_h
+    p '_____'
+
+    [["audi", "black", 2008], ["bmv", "red", 2014]].each do |car, color, year|
+      puts "car: #{car}, color: #{color}, year: #{year}"
+    end
+
+    p hash.empty?
+    p hash.size
+    p hash.keys
+
+    p hash.include? :car
+    p hash.sort
   end
 end
 
